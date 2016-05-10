a8a8f9e13f3e921397e66fcbef2adb9ee394d01c
Dawid <dawidjez@gmail.com>
2016-05-10 21:47:10 +0200

    update version

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var canvas,
     points   = [],
     webDraft = {
         title   : "WebDraft",
-        version : "2.1.6",
+        version : "2.2.0",
         click   : {
             left  : false, //left mouse button
             right : false  //right mouse button
