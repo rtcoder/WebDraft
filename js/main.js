87cadae27fc14046909bfae94490d0e67f404b42
Dawid Jeż <dawidjez@gmail.com>
2017-09-26 01:56:23 +0200

    v2.2.8

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ var canvas,
 
             isLoaded: false,
             title: "WebDraft",
-            version: "2.2.7",
+            version: "2.2.8",
             click: {
                 left: false, //left mouse button
                 right: false  //right mouse button
