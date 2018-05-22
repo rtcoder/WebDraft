c8a9bc0f662f6283dd4d8adcc25deaa31fa04768
Dawid Jeż <dawidjez@gmail.com>
2018-05-22 19:59:18 +0200

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    paths: ["./less"],
+                    yuicompress: true
+                },
+                files: {
+                    "./css/style.css": "./less/style.less"
+                }
+            }
+        },
+        watch: {
+            files: "./less/*",
+            tasks: ["less"]
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
