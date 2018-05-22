bb51a0a6b826c72617f075bdb12f4efe7f7aa85e
Dawid Jeż <dawidjez@gmail.com>
2018-05-22 21:03:02 +0200

    cssmin and concat js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,18 +4,51 @@ module.exports = function (grunt) {
             development: {
                 options: {
                     paths: ["./less"],
-                    yuicompress: true
+                    yuicompress: true,
+                    sourceMap: true
                 },
                 files: {
-                    "./css/style.css": "./less/style.less"
+                    "./build/css/style.css": "./less/style.less"
                 }
             }
         },
+        cssmin: {
+            options: {
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    './build/css/style.min.css': './build/css/style.css'
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: "\n",
+            },
+            dist: {
+                src: ["./js/modules/*", "./js/*"],
+                dest: './build/js/built.js',
+            },
+        },
         watch: {
-            files: "./less/*",
-            tasks: ["less"]
+            lessCompile: {
+                files: "./less/*",
+                tasks: ["less"]
+            },
+            minifyCss: {
+                files: "./build/css/style.css",
+                tasks: ["cssmin"]
+            },
+            concatJs: {
+                files: ["./js/*", "./js/modules/*"],
+                tasks: ["concat"]
+            },
         }
     });
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 };
